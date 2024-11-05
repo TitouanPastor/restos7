9ba1b619a82e28278a3331578d828ebf4672dfda
Titouan Pastor <titouan.pastor@gmail.com>
2024-11-05 15:41:20 +0100

    added props to the leaflet map

--- frontend/src/components/LeafletMap.vue
+++ frontend/src/components/LeafletMap.vue
@@ -1,20 +1,43 @@
 <template>
     <div>
-        <l-map :use-global-leaflet="false" ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+        <l-map :use-global-leaflet="false" ref="map" :zoom="zoom" :center="center">
             <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
                 name="OpenStreetMap"></l-tile-layer>
+            <!-- for each elements, get latitude and longitude to show as a marker -->
+            <l-marker v-for="element in elements" :key="element.Id_Restaurant" :lat-lng="[element.latitude, element.longitude]">
+                <l-popup>
+                    <Post :restaurant="element"/>
+                </l-popup>
+            </l-marker>
         </l-map>
     </div>
 </template>
 
 <script setup>
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
-import { ref } from 'vue';
+import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
+import Post from "./Post/Post.vue";
+import { ref } from "vue";
 
+// Définir les props avec des valeurs par défaut
+const props = defineProps({
+    center: {
+        type: Array,
+        default: () => [46.628237,  2.457814]
+    },
+    zoom: {
+        type: Number,
+        default: 6
+    },
+    elements: {
+        type: Array,
+        default: () => []
+    }
+});
 
-// Data
-const zoom = ref(5);
+// Réactiver les valeurs des props
+const center = ref(props.center);
+const zoom = ref(props.zoom);
 </script>
 
 <style scoped></style>
